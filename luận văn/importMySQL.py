d69d62b920df76b3bb0bf6ec3e0898c2cd7aeb60
tantoan97 <45889928+tantoan97@users.noreply.github.com>
2019-04-25 09:59:08 +0700

    luanvan 25/4/2019

---
+++ luận văn/importMySQL.py
@@ -0,0 +1,27 @@
+import PyMySQL
+#pymysql.install_as_MySQLdb()
+import xlrd
+db = PyMySQL.connect (host="localhost", user = "id8287156_tantoan97", passwd = "tantoan97", db = "output",charset="utf8", use_unicode=True)
+ 
+cursor = db.cursor()
+ 
+cursor.execute("SELECT VERSION()")
+ 
+data = cursor.fetchone()
+ 
+#print "Database version : %s " % data
+ 
+#import
+book = xlrd.open_workbook("predict.csv")
+sheet = book.sheet_by_name("predict")
+for r in range(0, sheet.nrows):    
+    sql = 'INSERT INTO stdio_inspirations(i_id,m_Content,m_Quotes,m_URL,m_Name)VALUES (NULL,"%s", "%s", "%s","%s")' % \
+        (sheet.cell(r,0).value,sheet.cell(r,1).value,sheet.cell(r,2).value,sheet.cell(r,3).value)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:   
+        db.rollback()
+ 
+ 
+db.close()
